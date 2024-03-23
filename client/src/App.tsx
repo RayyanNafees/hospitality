b0c9d43525ccba46d4c44778518b1b7b818be800
Rayyan Nafees <nafees.rayyan@gmail.com>
2024-03-23 21:27:59 +0530

    changed router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route, HashRouter } from 'react-router-dom'
+import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import Home from './pages/home'
 import Login from './pages/login'
 import SignUp from './pages/signup'
@@ -7,18 +7,18 @@ import Dashboard from './pages/dashboard'
 
 export default function App() {
   return (
-    <HashRouter basename='/'>
+    <BrowserRouter basename='/'>
       <Routes>
         <Route index path='/' element={<Home />} />
-        <Route index path='/guides' element={<Home />} />
-        <Route index path='/contact' element={<Home />} />
-        <Route index path='/about' element={<Home />} />
+        <Route path='/guides' element={<Home />} />
+        <Route path='/contact' element={<Home />} />
+        <Route path='/about' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/signin' element={<Login />} />
         <Route path='/signup' element={<SignUp />} />
         <Route path='/register' element={<SignUp />} />
         <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   )
 }
