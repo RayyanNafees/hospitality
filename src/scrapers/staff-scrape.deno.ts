e17fd54302ce52a1322c8c6fd9b3118cb91e048c
Rayyan Nafees <nafees.rayyan@gmail.com>
2024-05-07 18:34:36 +0530

    co

--- src/scrapers/staff-scrape.deno.ts
+++ src/scrapers/staff-scrape.deno.ts
@@ -1,34 +1,34 @@
-import {load, type Element} from 'cheerio'
+// import {load, type Element} from 'cheerio'
 
-const $ = await fetch(
-  'https://www.amu.ac.in/miscellaneous/jawaharlal-nehru-medical-college-hospital/staff-members'
-)
-  .then((r) => r.text())
-  .then(load)
+// const $ = await fetch(
+//   'https://www.amu.ac.in/miscellaneous/jawaharlal-nehru-medical-college-hospital/staff-members'
+// )
+//   .then((r) => r.text())
+//   .then(load)
 
-const data: Record<string, string | undefined>[] = []
+// const data: Record<string, string | undefined>[] = []
 
-$('.contact-info').each((_, el: Element) => {
-  const avatar = $(el).find('img').attr('src')
-  const name = $(el).find('.default-heading:first').find('h3').text()
-  const role = $(el).find('.default-heading:first').find('span').text()
-  const username = $(el).find('ul:first a').attr('href')?.split('/').at(-1)
+// $('.contact-info').each((_, el: Element) => {
+//   const avatar = $(el).find('img').attr('src')
+//   const name = $(el).find('.default-heading:first').find('h3').text()
+//   const role = $(el).find('.default-heading:first').find('span').text()
+//   const username = $(el).find('ul:first a').attr('href')?.split('/').at(-1)
 
-  const email = $(el).find('.details-area span:last').text()
-  const phone = $(el).find('ul:last li:first span').text()
+//   const email = $(el).find('.details-area span:last').text()
+//   const phone = $(el).find('ul:last li:first span').text()
 
-  data.push({
-    avatar,
-    name,
-    role,
-    username,
-    email,
-    phone,
-  })
-})
+//   data.push({
+//     avatar,
+//     name,
+//     role,
+//     username,
+//     email,
+//     phone,
+//   })
+// })
 
 
-// @ts-expect-error To be used in Deno Runtime ONLY!
-await Deno.writeTextFile('staff.json', JSON.stringify(data))
+// // @ts-expect-error To be used in Deno Runtime ONLY!
+// await Deno.writeTextFile('staff.json', JSON.stringify(data))
 
-console.log(data)
+// console.log(data)
--- src/scrapers/staff-slice.deno.ts
+++ src/scrapers/staff-slice.deno.ts
@@ -1,21 +1,21 @@
-import staff from './staff.json'        with {type: 'json'}
+// import staff from './staff.json'        with {type: 'json'}
 
-const SLICE_START = 0
-const SLICE_END = 18
+// const SLICE_START = 0
+// const SLICE_END = 18
 
-const slice = (
-  json: Array<Record<string, string>>,
-  slice_start: number = SLICE_START,
-  slice_end: number = SLICE_END
-) => {
-  if (!Array.isArray(json)) throw TypeError('Expected a JSON Array of Objects')
-  return json.slice(slice_start, slice_end)
-}
+// const slice = (
+//   json: Array<Record<string, string>>,
+//   slice_start: number = SLICE_START,
+//   slice_end: number = SLICE_END
+// ) => {
+//   if (!Array.isArray(json)) throw TypeError('Expected a JSON Array of Objects')
+//   return json.slice(slice_start, slice_end)
+// }
 
-const slicedStaff = slice(staff)
+// const slicedStaff = slice(staff)
 
-// @ts-expect-error Only to be used in deno runtime
-Deno.writeTextFile(
-  `staff-${SLICE_END - SLICE_START}.json`,
-  JSON.stringify(slicedStaff)
-)
+// // @ts-expect-error Only to be used in deno runtime
+// Deno.writeTextFile(
+//   `staff-${SLICE_END - SLICE_START}.json`,
+//   JSON.stringify(slicedStaff)
+// )
